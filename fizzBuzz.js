b68c303475cfc36b7f957e7a325dd7cc272a5fc8
Jordan Long <jordan.long222@gmail.com>
2016-05-24 19:49:35 -0400

    Show numbers only

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,169 +3,27 @@
 // For numbers which are multiples of both three and five print “FizzBuzz”." -- Imran Ghory
 // Your answer should print to the console.
 
-// var fb = ['', 'Fizz', 'Buzz', 'FizzBuzz'];
-var list = [];
+// //FIRST
+// // var fb = ['', 'Fizz', 'Buzz', 'FizzBuzz'];
+// var list = [];
+// for (var x = 0; x <= 100; x++){
+// 	console.log(x)
+//   if (x % 15 === 0)
+//   	console.log('FizzBuzz');
+//
+//   else if (x % 3 === 0) {
+//     console.log('Fizz');
+//   }
+//
+//   else if(x % 5 === 0) {
+//     console.log('Buzz');
+//   }
+// }
+
+//SECOND                            ONLY SHOWS THE NUMBERS WE WANT TO SEE
 for (var x = 0; x <= 100; x++){
-	console.log(x)
-  if (x % 15 === 0)
-  	console.log('FizzBuzz');
-
-  else if (x % 3 === 0) {
-    console.log('Fizz');
-  }
-
-  else if(x % 5 === 0) {
-    console.log('Buzz');
-  }
+	if (x%3!==0)
+	if (x%5!==0)
+	if (x%15!==0)
+		console.log(x);
 }
-
-//OUTCOME                Adds the strings to each when matched, but keeps the number :(
->0
-FizzBuzz
-1
-2
-3
-Fizz
-4
-5
-Buzz
-6
-Fizz
-7
-8
-9
-Fizz
-10
-Buzz
-11
-12
-Fizz
-13
-14
-15
-FizzBuzz
-16
-17
-18
-Fizz
-19
-20
-Buzz
-21
-Fizz
-22
-23
-24
-Fizz
-25
-Buzz
-26
-27
-Fizz
-28
-29
-30
-FizzBuzz
-31
-32
-33
-Fizz
-34
-35
-Buzz
-36
-Fizz
-37
-38
-39
-Fizz
-40
-Buzz
-41
-42
-Fizz
-43
-44
-45
-FizzBuzz
-46
-47
-48
-Fizz
-49
-50
-Buzz
-51
-Fizz
-52
-53
-54
-Fizz
-55
-Buzz
-56
-57
-Fizz
-58
-59
-60
-FizzBuzz
-61
-62
-63
-Fizz
-64
-65
-Buzz
-66
-Fizz
-67
-68
-69
-Fizz
-70
-Buzz
-71
-72
-Fizz
-73
-74
-75
-FizzBuzz
-76
-77
-78
-Fizz
-79
-80
-Buzz
-81
-Fizz
-82
-83
-84
-Fizz
-85
-Buzz
-86
-87
-Fizz
-88
-89
-90
-FizzBuzz
-91
-92
-93
-Fizz
-94
-95
-Buzz
-96
-Fizz
-97
-98
-99
-Fizz
-100
-Buzz
