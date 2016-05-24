232025b3694ed26389f2db97b59d94be28d50080
Jordan Long <jordan.long222@gmail.com>
2016-05-24 10:28:00 -0400

    2016.05.24.js

---
+++ 2016.05.24.js
@@ -0,0 +1,19 @@
+var children = [];
+var partner = [];
+var glocation = [];
+var job = [];
+
+var X = job;
+var Y = glocation;
+var Z = partner;
+var N = children;
+
+function saying {
+	document.write(You will be a', X, 'in', Y, ', and married to', Z, 'with', N, 'kids.)
+}
+
+var text = ['You will be a', X, 'in', Y, ', and married to', Z, 'with', N, 'kids.'];
+var fortune = text.map(
+	function(fortune) {
+		return
+	})
