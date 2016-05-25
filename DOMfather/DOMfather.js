113ecdf3b04d3e8f44460dc006e7376683be80b0
Jordan Long <jordan.long222@gmail.com>
2016-05-25 12:56:17 -0400

    Replace span w/ my info

--- DOMfather/DOMfather.js
+++ DOMfather/DOMfather.js
@@ -1,1 +1,5 @@
 document.body.style.fontFamily = 'sans-serif';
+document.getElementById('fullname').innerHTML = 'Jordan Long';
+document.getElementById('hometown').innerHTML = 'Detroit, MI';
+document.getElementById('movie').innerHTML = 'Finding Nemo';
+document.getElementById('food').innerHTML = 'Oreos';
