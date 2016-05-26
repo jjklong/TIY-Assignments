f49ce17fd10d621547ba4e9517c8ddff9ab42834
Jordan Long <jordan.long222@gmail.com>
2016-05-26 10:32:39 -0400

    Initial Commit

---
+++ lifetime_supply.js
@@ -0,0 +1,25 @@
+Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+
+Store your current age into a variable.
+
+Store a maximum age into a variable.
+
+Store an estimated amount per day (as a number).
+
+Calculate how many you would eat total for the rest of your life.
+
+Output the resut to the screen like so: "You will need NN to last you until the ripe old age of X".
+
+//X = maxAge - currentAge;
+//NN = X * oreosPerDay
+
+
+var currentAge = 25;
+var maxAge = 100;
+var oreosPerDay = 8;
+var totalOreos = [];
+
+var yearsLeft = [maxAge-currentAge];
+var amount = [yearsLeft * oreosPerDay];
+
+console.log("You will need " + 'amount' + "to last you until the ripe old age of " + 'yearsLeft' + ".");
