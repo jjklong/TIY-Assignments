ebf733135fcf28a93ca2810975e134afe73b4562
Jordan Long <jordan.long222@gmail.com>
2016-05-26 10:35:10 -0400

    Add console.log

--- lifetime_supply.js
+++ lifetime_supply.js
@@ -1,14 +1,14 @@
-Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
+// Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more!
 
-Store your current age into a variable.
+// Store your current age into a variable.
 
-Store a maximum age into a variable.
+// Store a maximum age into a variable.
 
-Store an estimated amount per day (as a number).
+// Store an estimated amount per day (as a number).
 
-Calculate how many you would eat total for the rest of your life.
+// Calculate how many you would eat total for the rest of your life.
 
-Output the resut to the screen like so: "You will need NN to last you until the ripe old age of X".
+// Output the resut to the screen like so: "You will need NN to last you until the ripe old age of X".
 
 //X = maxAge - currentAge;
 //NN = X * oreosPerDay
@@ -19,7 +19,7 @@ var maxAge = 100;
 var oreosPerDay = 8;
 var totalOreos = [];
 
-var yearsLeft = [maxAge-currentAge];
+var yearsLeft = [maxAge - currentAge];
 var amount = [yearsLeft * oreosPerDay];
 
-console.log("You will need " + 'amount' + "to last you until the ripe old age of " + 'yearsLeft' + ".");
+console.log("You will need " + [amount] + " to last you until the ripe old age of " + [yearsLeft] + ".");
