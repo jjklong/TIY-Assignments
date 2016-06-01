81e0735dcb555c6e147f7d358853ce836e0147f4
Jordan Long <jordan.long222@gmail.com>
2016-06-01 10:39:05 -0400

    Alt solution

--- pluralizeThat.js
+++ pluralizeThat.js
@@ -7,15 +7,35 @@
 // Call that function for a few different scores and log the result to make sure it works.
 
 // Bonus: Make it handle a few collective nouns like "sheep" and "geese".
+// 
+// function pluralizeThat (noun, number){
+// 	var plur = (" " + noun + "s");
+// 	var sing = (" " + noun);
+// 	if (number > 1 && noun !== ("sheep")) {
+// 		return number + plur;
+// 	} else {
+// 		return number + sing;
+// 	}
+// }
+//
+// console.log(pluralizeThat('toad', 2));
+// console.log(pluralizeThat('firefly', 1));
+// console.log(pluralizeThat('sheep', 8));
+// console.log(pluralizeThat('geese', 10));
+//
 
-function pluralizeThat (noun, number){
-	var plur = (" " + noun + "s");
-	var sing = (" " + noun);
-	if (number > 1) {
-		return number + plur;
-	} else {
-		return number + sing;
+// //////////////////////////////////
+//Hard Mode Solution//
+function pluralize (noun, number) {
+	if (number !=1 && noun != 'sheep' && noun != 'geese') {
+		return number + ' ' + noun + 's';
+	}
+
+	else {
+		return number + ' ' + noun;
 	}
 }
 
-console.log(pluralizeThat('frog', 2));
+console.log('I have ' + pluralize('frog', 2));
+console.log('I have ' + pluralize('sheep', 4));
+console.log('I have ' + pluralize('goose', 1));
