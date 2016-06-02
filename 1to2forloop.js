85ca497741020695870a829961577725deb2b566
Jordan Long <jordan.long222@gmail.com>
2016-06-02 11:24:23 -0400

    forloop initial commit

---
+++ 1to2forloop.js
@@ -0,0 +1,15 @@
+// Write a for loop that will iterate from 0 to 20.
+
+// For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g.j"2 is even").
+
+
+function OorE () {
+	var max = 20;
+for (i=0; i<=20; i++){
+	if (i%2===0){
+		console.log(i + " is even.");
+	} else
+		console.log(i + " is odd.");
+}
+}
+console.log(OorE());
