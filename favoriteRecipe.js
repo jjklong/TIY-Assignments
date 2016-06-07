59b97337b7eb6b4e166957fad21cca582075d67f
Jordan Long <jordan.long222@gmail.com>
2016-06-07 10:48:06 -0400

    exercise

---
+++ favoriteRecipe.js
@@ -0,0 +1,20 @@
+// Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+
+// On separate lines (one console.log statement for each), log the recipe information so it looks like:
+
+// Pizza
+// Serves: 2
+// Ingredients:
+// cinnamon
+// cumin
+// cocoa
+
+var favRecipe =
+	{title: "Pizza",
+	servings: 2,
+	ingredients: ("cheese", "onions", "bacon", "dough", "pepperoni", "marinara")
+	};
+
+
+
+console.log(favRecipe["title"]);
