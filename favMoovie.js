d1d1e634c4bb191d6cc434a6f1a656cb9853efe2
Jordan Long <jordan.long222@gmail.com>
2016-06-08 10:25:17 -0400

    initial commit favmoovie

---
+++ favMoovie.js
@@ -0,0 +1,27 @@
+// Create an object to store the following information about your favorite movie: title (a string), duraction (a number), and stars (an array of strings).
+
+// Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
+
+var favMoovie = {
+	"title": "Deadpool",
+	"duration": "108",
+	"Stars": [" Ryan Reynolds", " Morena Baccarin", " T.J. Miller"]
+	};
+
+console.log(favMoovie.title + " lasts for " + favMoovie.duration + " minutes. " + "Stars:" + favMoovie.Stars + ".");
+
+
+var title = favMoovie.title;
+console.log(title);
+var duration = favMoovie.duration;
+console.log(duration);
+var stars = favMoovie.Stars;
+console.log(stars);
+
+function movie (title, duration, stars) {
+	for (i = 0; i < favMoovie.length; i++) {
+
+
+
+	}
+}
